3ef71c25d1df38f59a10f777cd1ebdb9b8355f39
yisuoyanyv <840834298@qq.com>
2021-08-12 23:20:36 +0800

    react-redux工具的使用

---
+++ 7_src_react_redux_数据共享/redux/reducers/person.js
@@ -0,0 +1,17 @@
+import {ADD_PERSON} from '../constant'
+
+//初始化人的列表
+const initState=[{id:'001',name:'tom',age:18}]
+export default function personReducer(preState = initState,action){
+    const {type,data} = action
+    console.log('personReducer@#@#')
+    switch (type) {
+        case ADD_PERSON:
+            //preState.unshift(data)  //此处不可以这样写，这样会导致preState被改写了，personReducer就不是纯函数了
+            return [data,...preState]   
+        default:
+            return preState;
+    }
+
+
+}
--- 7_src_react_redux_数据共享/redux/store.js
+++ 7_src_react_redux_数据共享/redux/store.js
@@ -11,15 +11,12 @@ import personReducer  from './reducers/person'
 //引入redux-thunk，用于支持异步action
 import thunk  from 'redux-thunk'
 
-//引入redux-devtools-extension
-import {composeWithDevTools} from 'redux-devtools-extension'
-
 //汇总所有的reducer变为一个总的reducer
 const allReducer =combineReducers({
     he:countReducer,
     rens:personReducer
 })
-//暴露store  applyMiddleware(thunk)
-export default  createStore(allReducer,composeWithDevTools(applyMiddleware(thunk)))
+//暴露store
+export default  createStore(allReducer,applyMiddleware(thunk))
 
 
